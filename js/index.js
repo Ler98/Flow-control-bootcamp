12eaf6283cb61b008cbb4b058abc68126f30f7db
Ler98 <lisa.eriksson98@hotmail.com>
2025-05-13 22:29:54 +0200

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+//let a = 20;
+//let b = 20;
+
+
+/*if (b =="20") {
+}
+    console.log("banan");
+if (a =="10") {
+    console.log("apple");
+}
+*/
+
+//om a är större än b skriv ut a
+//om b är större än a skriv ut b
+
+let a = 30;
+let b = 30;
+
+
+if (a > b) {
+    console.log("apple");
+}
+else if (b > a) {
+    console.log("pear");
+}
+
+
+
+if (a == b) {
+    console.log(50); //skriver ut 40 om a är samma som b
+}
+
+
+function add(numberOne, numberTwo) {
+    console.log("numberOne är: ", numberOne);
+    console.log("numberTwo är: ", numberTwo);
+    let sum = numberOne + numberTwo;
+
+    console.log("Summan blir " + sum);
+}
+
+add(6,11);
+add(25, 55);
